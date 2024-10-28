ebae932ab9b6c4c175b89f454b8a7a50f1997af6
fuyuookura <fuyuookura@163.com>
2024-10-28 16:59:45 -0400

    finish

--- README.md
+++ README.md
@@ -1,60 +1,7 @@
-# p5.js Template
+For this week's assignment, I want to use the word I saw in Oppenheimer, Divi sooryasahasrasya bhavedyugapadutthitaa; Yadi bhaah sadrishee saa syaadbhaasastasya mahaatmanah.
+This quote is from the Bhagavad Gita, Chapter 11, Verse 12. he referenced this verse after the successful test of the first atomic bomb in the United States in 1945.
+This Sanskrit verse can be translated as:
+"If the radiance of a thousand suns were to burst at once into the sky, that would be like the splendor of the mighty one."
 
-This is a README file that can be used to describe and document your assignment.
-
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
-
----
----
-
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
-
-**bold text**
-
-*italicized text*
-
-~~strikethrough text~~
-
-Ordered List:
-1. First item
-2. Second item
-3. Third item
-
-Unordered List:
-- First item
-- Second item
-- Third item
-
-`short code block`
-
-```
-extended code block
-fun() {
-  return 0
-}
-```
-
-Link:  
-[linked text](https://www.example.com)
-
-
-Image with url:  
-![image description](https://dm-gy-6063-2024f-b.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
-
-
-Image on repo:  
-![image description](./file-name.jpg)
-
-
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
+I want to use this word to monitor the explotion scene, click the bomb emoji the screen will change, and hence the sentence will scatter, click the word will have sun emoji appear in the screen.
+![alt text](image-1.png)
--- sketch.js
+++ sketch.js
@@ -1,6 +1,85 @@
+let verse;
+let words = [];
+let exploded = false;
+let sunPositions = [];
+let sunEmojis = [];
+let explodeImage;
+
+function preload() {
+  verse = loadStrings('verse.txt');
+  explodeImage = loadImage('explode.jpg');
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textFont('Great Vibes');
+  textAlign(CENTER, CENTER);
+
+  verse = join(verse, ' ');
+  
+  words = verse.split(' ');
 }
 
 function draw() {
+  if (!exploded) {
+    background(200);
+    fill(0);
+    let middleIndex = Math.floor(words.length / 2);
+    let firstLine = words.slice(0, middleIndex).join(' ');
+    let secondLine = words.slice(middleIndex).join(' ');
+    textSize(60);
+    text(firstLine, width / 2, height / 2 - 40);
+    text(secondLine, width / 2, height / 2);
+
+    push();
+    textSize(100);
+    text('💣', width / 2, height / 2 + 100);
+    pop();
+
+  } else {
+
+    background(explodeImage);
+
+    for (let i = 0; i < words.length; i++) {
+      fill(0);
+      push(); 
+      textSize(60);
+      text(words[i], sunPositions[i].x, sunPositions[i].y);
+      pop();
+    }
+
+    for (let i = 0; i < sunEmojis.length; i++) {
+      textSize(60);
+      text('☀️', sunEmojis[i].x, sunEmojis[i].y);
+    }
+  }
+}
+
+function mousePressed() {
+  
+  if (!exploded) {
+    exploded = true;
+    sunPositions = []; 
+
+    
+    for (let i = 0; i < words.length; i++) {
+      let x = random(width * 0.1, width * 0.9);
+      let y = random(height * 0.1, height * 0.9);
+      sunPositions.push(createVector(x, y));
+    }
+  } else {
+    
+    for (let i = 0; i < sunPositions.length; i++) {
+      let d = dist(mouseX, mouseY, sunPositions[i].x, sunPositions[i].y);
+      if (d < 50) { 
+
+        for (let j = 0; j < 4; j++) { 
+          let x = random(width);
+          let y = random(height);
+          sunEmojis.push(createVector(x, y));
+        }
+
+      }
+    }
+  }
 }
